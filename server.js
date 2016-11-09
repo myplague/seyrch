973701e0f9d75652479aa7a64650d77f81514a93
Ernesto Guzman Ferreriro <Ernesto Guzman Ferreriro>
2016-11-08 22:05:17 -0500

    listo pero lo puedo mejorar

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const Yelp = require('yelp');
 const OAuth = require("oauth");
-const YELP_KEY = "DrSxFHaExEG7JnvfplXKWA";
-const YELP_SECRET = "0OHyGS6twi3SrQboaY26tHJGlr4";
-const YELP_TOKEN = "BOTZhZwJEH0V05sK8Isu3VGpTGsSjpG8"
-const YELP_TOKENSECRET = "uLHRQlxpAUadPG1O3Tidssd43Xo";
+const YELP_KEY = "bMoiQT7XuLt8bz61R6KkeA";
+const YELP_SECRET = "CCmAqq7bq6JP0EHNH_-H7Cvtvnw";
+const YELP_TOKEN = "QOCGK9hYtfM4vLZU_b-Cr0-Vm_IXk5yX"
+const YELP_TOKENSECRET = "zLX63Act4e31-lsGBEIpMbydXsk";
 // const cors = require(`cors`);
 
 // app.use(cors());
@@ -13,10 +13,13 @@ const YELP_TOKENSECRET = "uLHRQlxpAUadPG1O3Tidssd43Xo";
 //
 app.get("/", function(req,res){
   console.log("received request");
-  res.send("Hello World");
+  res.sendFile(__dirname + "/webpage/index.html");
   //to generate a random number, the code would be:
     // res.send("" + Math.floor(Math.random()*9));
-})
+});
+app.get('/css/style.css', function(req, res){
+  res.sendFile(__dirname + '/webpage/css/style.css');
+});
 
 app.get("/random/:start/:end", function(req,res){
   var start = req.params.start;
@@ -42,6 +45,20 @@ yelp.search({ term: 'sushi', location: 'Miami, FL' })
   res.send(error);
 });
 });
+app.get("/seyrch/:city/:term/", function (req, res) {
+  var yelp = new Yelp({
+  consumer_key: YELP_KEY,
+  consumer_secret: YELP_SECRET,
+  token: YELP_TOKEN,
+  token_secret: YELP_TOKENSECRET,
+  });
+  yelp.search({ term: req.params.term, location: req.params.city })
+  .then(function (data) {
+    console.log(JSON.stringify(data,null, 2));
+    res.json(data);
+  });
+});
+
  // app.get("/laughing", function(req,res){
 //   res.send("hahahahhaha");
 // });
